6ef1952ecc2c9d161cdf4dc34b8054fa28229f7a
Kabir5296 <manb5296@gmail.com>
2025-11-18 00:58:04 -0500

    prod

--- submit_train_task0_ucf101.sh
+++ submit_train_task0_ucf101.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=task0_ucf
 #SBATCH --partition=course_gpu
-#SBATCH --gres=gpu:a100_40g:1
+#SBATCH --gres=gpu:a100_10g:1
 #SBATCH --account=2025-fall-ds-677-amr239-ak3535
 #SBATCH --qos=course
 #SBATCH --time=6:00:00
@@ -9,14 +9,14 @@
 #SBATCH --error=./job_logs/ucf101/task0/%x.%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --mem=16G
 
 # module load Miniforge3
 conda activate ds_project
 module load CUDA
 
 TRAIN_CONFIG_FILE="config/train_configs/UCF101/train_task0.yml"
-OUTPUT_FOLDER="model_save/ucf101/task0"
+OUTPUT_FOLDER="model_save/try1"
 
 python train_task_0.py \
     --config "$TRAIN_CONFIG_FILE" \
