68c7f7167fb691ad5462378845db4f9c9c890e2e
Kabir5296 <manb5296@gmail.com>
2025-12-02 18:57:48 -0500

    task2

---
+++ submit_finetune_task2_ucf101.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH --job-name=task2_ucf
+#SBATCH --partition=course_gpu
+#SBATCH --gres=gpu:a100_10g:1
+#SBATCH --account=2025-fall-ds-677-amr239-ak3535
+#SBATCH --qos=course
+#SBATCH --time=70:00:00
+#SBATCH --output=./job_logs/ucf101/task2/%x.%j.out
+#SBATCH --error=./job_logs/ucf101/task2/%x.%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=30G
+
+# module load Miniforge3
+conda activate ds_project
+module load CUDA
+
+TRAIN_CONFIG_FILE="config/train_configs/UCF101/train_task2.yml"
+OUTPUT_FOLDER="model_save/smaller_model"
+
+python fine_tune.py \
+    --config "$TRAIN_CONFIG_FILE" \
+    --save_folder "$OUTPUT_FOLDER"
